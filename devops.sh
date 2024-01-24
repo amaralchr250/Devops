d2779cc5d69bd2d87cfb33c8ef1d22d59e54ed51
Christopher Amaral <42553791+amaralchr250@users.noreply.github.com>
2024-01-24 06:02:34 -0300

    Update devops.sh

--- devops.sh
+++ devops.sh
@@ -25,12 +25,6 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.
 sudo apt-get update
 sudo apt-get install helm
 
-# Instalar Minikube
-status "Instalando Minikube"
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-minikube start --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus 1
-
 # Instalar Docker
 status "Instalando Docker"
 sudo apt-get update
@@ -41,6 +35,12 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Instalar Minikube
+status "Instalando Minikube"
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus 1
 
 # Instalar kubectl
 status "Instalando kubectl"
@@ -60,16 +60,16 @@ docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /va
 status "Subindo container MySQL"
 docker run -d -p 3306:3306 --name mysql-A -e MYSQL_ROOT_PASSWORD=Senha123 -e MYSQL_DATABASE=devops -e MYSQL_USER=admin -e MYSQL_PASSWORD=Senha123 mysql/mysql-server:latest
 
+# Subir cluster ArgoCD
+status "Subindo cluster ArgoCD"
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
 # Instalar GUI e server para acesso remoto
 status "Instalando GUI e servidor para acesso remoto"
 sudo apt update
 sudo apt install x2goserver x2goserver-xsession
 sudo apt install mate-core mate-desktop-environment mate-notification-daemon
-
-# Subir cluster ArgoCD
-status "Subindo cluster ArgoCD"
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # Mensagem de conclusão
 status "Instalações concluídas com sucesso!"
